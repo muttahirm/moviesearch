b9c1c54f53fd3c5eff4190832dfd9e253298fb99
Muttahir Mumtaz <mumtaz.muttahir@gmail.com>
2024-04-16 20:31:24 +0200

    architecture

---
+++ lib/presentation/features/search/search_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Movies Search'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/presentation.dart
@@ -0,0 +1,2 @@
+export 'features/features.dart';
+export 'shared_ui/shared_ui.dart';
